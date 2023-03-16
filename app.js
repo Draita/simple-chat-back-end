fc98e0d3720d2528cfd2a7c713913d11ec3e30b0
Thijmen <yeaaccount@gmail.com>
2023-03-16 08:26:33 +0100

    added socket and new routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const http = require('http');
+
 const express = require('express')
 const errorHandler = require('./middlewares/errorHandler');
 const mongoose = require('mongoose');
@@ -7,9 +9,23 @@ const cors = require('cors');
 
 
 
-const authMiddleware = require('./middlewares/authMiddleware');
+const {authMiddleware} = require('./middlewares/authMiddleware');
 const app = express()
 const port = 3000
+
+const server = http.createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    credentials: true
+  }
+});
+
+// Initialize socket.io
+require('./sockets/roomSocket')(io);
+
+
 
 
 app.use(cors({
@@ -21,14 +37,22 @@ app.use(cors({
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
+
+app.use(cookieParser())
 
 
 app.use(cookieParser())
 // ROUTES IMPORTS
 const authRoutes = require('./routes/auth');
 const friendRoutes = require('./routes/friend');
+const roomRoutes = require('./routes/room');
+const userRoutes = require('./routes/userRoute');
+
+
 
 
 app.post('/', (req, res) => {
@@ -39,28 +63,26 @@ app.post('/', (req, res) => {
 
 
 // define routes
+app.use('/users',authMiddleware, userRoutes);
+
 app.use('/friend',authMiddleware, friendRoutes);
+app.use('/room',authMiddleware, roomRoutes);
+
 
 app.use('/', authRoutes);
 
-app.use(errorHandler);
 
+app.use(errorHandler);
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
+
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.log('Failed to connect to MongoDB', err);
 });
-
 const db = mongoose.connection;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+server.listen(port, () => console.log(`Server running on port ${port}`));
