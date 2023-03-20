06b97d3297e9f3da4bdfaa4ece4b6b184b11725b
Thijmen <yeaaccount@gmail.com>
2023-03-20 16:02:01 +0100

    added that roomchatfunctioanlity sockets

---
+++ sockets/roomChatFunctionalitySocket.js
@@ -0,0 +1,36 @@
+const {
+  loadMoreMessages,
+  leaveRoom,
+  createNotification,
+  joinRoom,
+  saveMessage
+} = require("../controllers/roomController");
+
+module.exports = (io) => {
+  io.on("connection", async (socket) => {
+    console.log(`User connected: ${socket.user._id}`);
+
+    socket.on("join room", async (roomId) => {
+      await joinRoom(io, socket, roomId);
+    });
+
+    socket.on("leave room", () => {
+      leaveRoom(socket);
+    });
+
+    socket.on("new message", async (message) => {
+
+      const savedMessage = await saveMessage(message, socket.user, socket.roomId);
+      await createNotification(socket, savedMessage, io);
+      io.to(socket.roomId).emit("new message", savedMessage);
+    });
+
+    socket.on("load more messages", async (lastMessageId) => {
+      await loadMoreMessages(socket, lastMessageId);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.user._id}`);
+    });
+  });
+};
