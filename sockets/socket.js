a391dcf4c13aa57009811b100c1cbab0c427c4c2
Thijmen <yeaaccount@gmail.com>
2023-03-22 13:10:57 +0100

    added new socket and changed connect log

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,33 +1,38 @@
 const { socketAuthMiddleware } = require("../middlewares/authMiddleware");
 const roomChatFunctionalitySocket = require("./roomChatFunctionalitySocket");
 const roomListSocket = require("./roomListSocket");
+const friendSocket = require("./friendSocket");
+
 const User = require("../models/userModel");
 
 module.exports = (io) => {
   io.use(socketAuthMiddleware);
 
   io.on("connection", async (socket) => {
-    console.log("User connected");
-
     // Update user's isOnline status to true and add socket ID to list
     const user = await User.findByIdAndUpdate(
       socket.user._id,
       { $set: { isOnline: true }, $push: { connectedSockets: socket.id } },
       { new: true }
     );
+    console.log("User connected: "+user.username);
+
 
     roomChatFunctionalitySocket(io, socket);
     roomListSocket(io, socket);
+    friendSocket(io, socket);
+
 
     socket.on("disconnect", async () => {
-      console.log("User disconnected");
-
+      console.log("hereby the user is disconnect:"+socket.user.username)
       // Update user's isOnline status to false and remove socket ID from list
       const updatedUser = await User.findByIdAndUpdate(
-        user._id,
+        socket.user._id,
         { $set: { isOnline: false }, $pull: { connectedSockets: socket.id } },
         { new: true }
       );
+
+
     });
   });
 };
